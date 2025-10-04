0b812a504bedc49738bc43ab6efc2139597190f8
Junhyeong Choi <junhyung9571@naver.com>
2025-10-04 17:10:58 +0900

    [Feat] Convert SkeletonNode

---
+++ datatypes/skeleton_node.py
@@ -0,0 +1,64 @@
+from .transform import Transform
+
+class SkeletonNode:
+    def __init__(self, name, father, local_t: Transform, global_t: Transform):
+        self._name = name
+        self._father = father  # Index of the father node
+        self.next = []  # Index of the attached nodes
+
+        # Transformations
+        self._local_t_current = local_t
+        self._global_t_current = global_t
+
+        self._local_t_rest = local_t.copy()
+        self._global_t_rest = global_t.copy()
+
+        self._global_t = Transform()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def father(self):
+        return self._father
+
+    @property
+    def local_t_current(self):
+        return self._local_t_current
+
+    @local_t_current.setter
+    def local_t_current(self, value):
+        self._local_t_current = value
+
+    @property
+    def global_t_current(self):
+        return self._global_t_current
+
+    @global_t_current.setter
+    def global_t_current(self, value):
+        self._global_t_current = value
+
+    @property
+    def local_t_rest(self):
+        return self._local_t_rest
+
+    @local_t_rest.setter
+    def local_t_rest(self, value):
+        self._local_t_rest = value
+
+    @property
+    def global_t_rest(self):
+        return self._global_t_rest
+
+    @global_t_rest.setter
+    def global_t_rest(self, value):
+        self._global_t_rest = value
+
+    @property
+    def global_t(self):
+        return self._global_t
+
+    @global_t.setter
+    def global_t(self, value):
+        self._global_t = value
