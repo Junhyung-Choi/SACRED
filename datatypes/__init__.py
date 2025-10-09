220a9dc5bdc30cf673d1642c6c01c075b3ee1d84
Junhyeong Choi <junhyung9571@naver.com>
2025-10-09 14:42:00 +0900

    [Chore] Set __init__.py for datatypes

---
+++ datatypes/__init__.py
@@ -0,0 +1,11 @@
+from .bbox import BoundingBox
+from .cage import Cage
+from .character import Character
+from .quaternion import Quaternion
+from .skeleton_node import SkeletonNode
+from .skeleton import Skeleton
+from .transform import Transform
+from .trimesh import Trimesh
+from .weights import Weights
+
+__all__ = [BoundingBox, Cage, Character, Quaternion, SkeletonNode, Skeleton, Transform, Trimesh, Weights]
